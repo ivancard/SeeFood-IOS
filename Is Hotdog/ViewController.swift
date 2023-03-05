2afbccde1da66b87497cfe4a1b4e1c70bc069914
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2023-03-05 17:15:19 -0300

    initial Commit

--- Is Hotdog/ViewController.swift
+++ Is Hotdog/ViewController.swift
@@ -14,16 +14,20 @@ class ViewController: UIViewController {
     let imagePicker = UIImagePickerController()
 
     @IBOutlet weak var imagePlaceHolder: UIImageView!
+    @IBOutlet weak var isHotDogView: UIView!
+    @IBOutlet weak var notHotDogView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         imagePicker.delegate = self
-        imagePicker.sourceType = .photoLibrary
+        imagePicker.sourceType = .camera
         imagePicker.allowsEditing = false
     }
 
     @IBAction func cameraAction(_ sender: Any) {
+        self.notHotDogView.isHidden = true
+        self.isHotDogView.isHidden = true
         present(imagePicker, animated: true)
     }
 }
@@ -38,7 +42,39 @@ extension ViewController: UIImagePickerControllerDelegate {
 
         imagePlaceHolder.image = image
 
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Could not convert to ciImage")
+        }
+        detect(image: ciImage)
         imagePicker.dismiss(animated: true)
+    }
+
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: Inceptionv3.urlOfModelInThisBundle)) else {
+            fatalError("Can't load ML model no")
+        }
+
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process the image")
+            }
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.notHotDogView.isHidden = true
+                    self.isHotDogView.isHidden = false
+                } else {
+                    self.notHotDogView.isHidden = false
+                    self.isHotDogView.isHidden = true
+                }
+            }
+        }
+
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
     }
 }
 
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This use the camera or the photo library and reconize if the picture is or not a HotDog
 
 - Using CoreML, apple API for machine learning
+
+## Demostration
+
+![app running](Documentation/appGif.gif)
